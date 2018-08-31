16182bdee649f3e233506e0afb625c633514c14a
lurenjie <lurenjie@cashbus.com>
2018-08-31 16:14:22 +0800

    加上Zookeeper自动重连

--- trpc-register/src/main/java/com/tracy/trpc/register/register/ZookeeperRegister.java
+++ trpc-register/src/main/java/com/tracy/trpc/register/register/ZookeeperRegister.java
@@ -8,22 +8,30 @@ import com.tracy.trpc.common.zookeeper.ZookeeperFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.imps.CuratorFrameworkState;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 
 import javax.annotation.PreDestroy;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lurenjie on 2017/6/8
  */
 @Slf4j
-public class ZookeeperRegister implements Register {
+public class ZookeeperRegister implements Register, ConnectionStateListener {
     private CuratorFramework zkClient;
+    private List<NodeInfo> serverPath;
 
     public ZookeeperRegister() throws Exception {
         if (zkClient == null) {
             zkClient = new ZookeeperFactory().getObject();
         }
+        serverPath = new ArrayList<>(10);
+        zkClient.getConnectionStateListenable().addListener(this);
     }
 
     /**
@@ -37,10 +45,8 @@ public class ZookeeperRegister implements Register {
         if (zkClient.getState() == CuratorFrameworkState.LATENT) {
             zkClient.start();
         }
-        String zkPath = CommonUtil.getZkPath(nodeInfo);
-        zkClient.create().creatingParentsIfNeeded()
-                .withMode(CreateMode.EPHEMERAL)
-                .forPath(zkPath, JSON.toJSONString(nodeInfo).getBytes(Charset.forName("UTF-8")));
+        serverPath.add(nodeInfo);
+        createdNode(nodeInfo);
         log.info("=======service :{} registry finish!=======", nodeInfo.getApplicationName());
 
     }
@@ -64,6 +70,36 @@ public class ZookeeperRegister implements Register {
     public void destroy() {
         if (zkClient != null) {
             zkClient.close();
+        }
+    }
+
+    @Override
+    public void stateChanged(CuratorFramework client, ConnectionState newState) {
+        if (newState == ConnectionState.LOST) {
+            try {
+                for (NodeInfo info : serverPath) {
+                    Stat stat = zkClient.checkExists().forPath(CommonUtil.getZkPath(info));
+                    if (stat == null) {
+                        createdNode(info);
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Deal connect lost error!", e);
+            }
+        }
+    }
+
+    private void createdNode(NodeInfo info) {
+        String zkPath = CommonUtil.getZkPath(info);
+        while (true) {
+            try {
+                zkClient.create().creatingParentsIfNeeded()
+                        .withMode(CreateMode.EPHEMERAL)
+                        .forPath(zkPath, JSON.toJSONString(info).getBytes(Charset.forName("UTF-8")));
+                break;
+            } catch (Exception e) {
+                log.error("create zk node error", e);
+            }
         }
     }
 }
